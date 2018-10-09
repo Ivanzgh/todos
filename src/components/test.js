3432ea9d1a61cd4f0edcac9381a2bae84d3e1672
zghIvan <1972571253@qq.com>
2018-10-09 09:10:25 +0800

    Delete test.js

--- src/components/test.js
+++
@@ -1,9 +0,0 @@
-fetch('https://www.npmjs.com/search?q=keywords:url', {
-    method: 'GET'
-})
-    .then(res => {
-        return res.text();
-    })
-    .then(res => {
-        console.log(res)
-    });
